017c0bb9e5d15261cbd7f9dd63c7d9e15d5b9de8
janisollo <sollo.jani@gmail.com>
2023-02-20 17:41:47 +0200

    All routes added

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -2,7 +2,16 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.json({ message: "Hello there" });
+  res.json({ message: "Hello everyolfdhkpne" });
+});
+router.post("/", (req, res) => {
+  res.json({ message: "Set Goal" });
+});
+router.put("/:id", (req, res) => {
+  res.json({ message: `Update goal ${req.params.id}` });
+});
+router.delete("/:id", (req, res) => {
+  res.json({ message: `Delete goal ${req.params.id}` });
 });
 
 module.exports = router;
